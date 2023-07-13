3f8525fd3cd7e49abf8961e75ecb3851d18852b7
Ahmed <ahmedbentaj710@gmail.com>
2023-07-13 11:59:02 +0100

    Add articlevente

---
+++ OzonExpress/OzonExpress/Models/ArticleVente.cs
@@ -0,0 +1,11 @@
+﻿namespace OzonExpress.Models
+{
+    public class ArticleVente
+    {
+        public int ArticleId { get; set; }
+        public int VenteId { get; set; }
+        public int Quantite { get; set; }
+        public Article Article { get; set; }
+        public Vente Vente { get; set; }
+    }
+}
