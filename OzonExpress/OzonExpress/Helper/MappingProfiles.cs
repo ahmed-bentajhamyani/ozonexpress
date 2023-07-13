f4fae4a3134c942d8d8c98acab7d66e389b619eb
Ahmed <ahmedbentaj710@gmail.com>
2023-07-13 11:59:38 +0100

    Automapper profiles

---
+++ OzonExpress/OzonExpress/Helper/MappingProfiles.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using OzonExpress.Dto;
+using OzonExpress.Models;
+using System.Diagnostics.Metrics;
+
+namespace OzonExpress.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Article, ArticleDto>();
+            CreateMap<ArticleDto, Article>();
+
+            CreateMap<Blog, BlogDto>();
+            CreateMap<BlogDto, Blog>();
+
+            CreateMap<FAQ, FAQDto>();
+            CreateMap<FAQDto, FAQ>();
+
+            CreateMap<Tarif, TarifDto>();
+            CreateMap<TarifDto, Tarif>();
+
+            CreateMap<Client, ClientDto>();
+            CreateMap<ClientDto, Client>();
+
+            CreateMap<Vente, VenteDto>();
+            CreateMap<VenteDto, Vente>();
+
+            CreateMap<Panier, PanierDto>();
+            CreateMap<PanierDto, Panier>();
+
+            CreateMap<Ville, VilleDto>();
+            CreateMap<VilleDto, Ville>();
+        }
+    }
+}
