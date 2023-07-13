f1b594fc5cd5976ec857ef5622c4109cfb2bfcde
Ahmed <ahmedbentaj710@gmail.com>
2023-07-13 11:58:54 +0100

    Add articlepanier

---
+++ OzonExpress/OzonExpress/Models/ArticlePanier.cs
@@ -0,0 +1,11 @@
+﻿namespace OzonExpress.Models
+{
+    public class ArticlePanier
+    {
+        public int ArticleId { get; set; }
+        public int PanierId { get; set; }
+        public int Quantite { get; set; }
+        public Article Article { get; set; }
+        public Panier Panier { get; set; }
+    }
+}
