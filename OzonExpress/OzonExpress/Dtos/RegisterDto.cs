087416ce8f94bb77b3121c564f503929097e21d5
Ahmed <ahmedbentaj710@gmail.com>
2023-08-19 15:02:05 +0100

    Dto to Dtos

---
+++ OzonExpress/OzonExpress/Dtos/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OzonExpress.Dto
+{
+    public class RegisterDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
